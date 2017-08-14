6328a2fc514e78a0902d72d74538fa03f29963f8
RaiAlysa <30695483+RaiAlysa@users.noreply.github.com>
2017-08-14 19:20:52 +0200

    Delete test.py

--- virtual-Assistant/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("/usr/local/lib/python2.7/site-packages/")
-from nltk import word_tokenize
-import nltk
-from nltk.corpus import stopwords
-import os
-os.system('gtts-cli.py "Hello my master how are you today?" -l en -o hello.mp3')
-os.system("mpg321 hello.mp3")
-os.system("rm hello.mp3")
